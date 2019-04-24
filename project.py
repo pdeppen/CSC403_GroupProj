a3da305061d246caa7f617f43473d24146d0dbee
pdeppen <pdeppen17@aol.com>
2019-04-24 17:26:51 -0400

    data is imported

---
+++ project.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+
+def load_credit_data():
+    csv_path = os.path.join("./datasets/dataset_31_credit-g.csv")
+    return pd.read_csv(csv_path)
+
+credit_data = load_credit_data()
+print(credit_data.head())
+
