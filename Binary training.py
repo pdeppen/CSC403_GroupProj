05cb8981d53d2fa2771b66a0f5cee9cbd93d3242
pdeppen <pdeppen17@aol.com>
2019-05-03 16:07:27 -0400

    added screenshots

--- Binary training.py
+++ Binary training.py
@@ -95,5 +95,19 @@ def plot_precision_vs_recall(precisions, recalls):
     plt.axis([0,1,0,1])
 
 
-plot_precision_vs_recall(precisions, recalls)
+# plot_precision_vs_recall(precisions, recalls)
+
+from sklearn.metrics import roc_curve
+
+fpr, tpr, thresholds = roc_curve(y_train_yes, y_scores)
+
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0,1], [0,1], 'k--')
+    plt.axis([0,1,0,1])
+    plt.xlabel('False Positive Rate', fontsize=16)
+    plt.ylabel('True Positive Rate', fontsize=16)
+
+
+plot_roc_curve(fpr, tpr)
 plt.show()
