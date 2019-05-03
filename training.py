52ce2be6419e9652ae27354a8bba32e239bb051c
pdeppen <pdeppen17@aol.com>
2019-05-03 15:57:31 -0400

    added precision and recall

--- training.py
+++ training.py
@@ -17,17 +17,17 @@ credit_data = load_credit_data()
 credit_data = credit_data.apply(LabelEncoder().fit_transform)
 
 # dropping features
-credit_data = credit_data.drop("num_dependents", axis=1)
-credit_data = credit_data.drop("purpose", axis=1)
-credit_data = credit_data.drop("personal_status", axis=1)
-credit_data = credit_data.drop("other_parties", axis=1)
-credit_data = credit_data.drop("property_magnitude", axis=1)
-credit_data = credit_data.drop("other_payment_plans", axis=1)
-credit_data = credit_data.drop("existing_credits", axis=1)
-credit_data = credit_data.drop("job", axis=1)
-credit_data = credit_data.drop("own_telephone", axis=1)
-credit_data = credit_data.drop("foreign_worker", axis=1)
-credit_data = credit_data.drop("installment_commitment", axis=1)
+# credit_data = credit_data.drop("num_dependents", axis=1)
+# credit_data = credit_data.drop("purpose", axis=1)
+# credit_data = credit_data.drop("personal_status", axis=1)
+# credit_data = credit_data.drop("other_parties", axis=1)
+# credit_data = credit_data.drop("property_magnitude", axis=1)
+# credit_data = credit_data.drop("other_payment_plans", axis=1)
+# credit_data = credit_data.drop("existing_credits", axis=1)
+# credit_data = credit_data.drop("job", axis=1)
+# credit_data = credit_data.drop("own_telephone", axis=1)
+# credit_data = credit_data.drop("foreign_worker", axis=1)
+# credit_data = credit_data.drop("installment_commitment", axis=1)
 
 # convert to array
 credit_data = credit_data.values
@@ -36,11 +36,11 @@ credit_data = credit_data.values
 credit_data_data = credit_data
 
 # remove target column from data set
-credit_data_data = np.delete(credit_data_data, 9, axis=1)
+credit_data_data = np.delete(credit_data_data, 20, axis=1)
 
 # target set
 credit_data_target = credit_data
-credit_data_target = np.delete(credit_data_target, np.s_[0:9], axis=1)
+credit_data_target = np.delete(credit_data_target, np.s_[0:20], axis=1)
 credit_data_target = credit_data_target.ravel()
 
 
@@ -53,15 +53,47 @@ x_train, y_train = x_train[shuffle_index], y_train[shuffle_index]
 y_train_yes = (y_train == 1)
 y_test_yes = (y_test == 1)
 
-sgd_clf = SGDClassifier(max_iter=20, tol=np.infty, random_state=42)
+sgd_clf = SGDClassifier(max_iter=20, tol=np.infty)
 sgd_clf.fit(x_train, y_train_yes)
 
-print(cross_val_score(sgd_clf, x_train, y_train_yes, cv=3, scoring="accuracy"))
+# print(cross_val_score(sgd_clf, x_train, y_train_yes, cv=2, scoring="accuracy"))
 
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import f1_score
 
 y_train_pred = cross_val_predict(sgd_clf, x_train, y_train_yes, cv=3)
 
-print(confusion_matrix(y_train_yes, y_train_pred))
+# print(confusion_matrix(y_train_yes, y_train_pred))
+# print(sgd_clf.predict([credit_data_data[1]]))
 
+# precision
+from sklearn.metrics import precision_score, recall_score
+print("Precision Score: ", precision_score(y_train_yes, y_train_pred))
+print("Recall Score: ", recall_score(y_train_yes, y_train_pred))
+
+print("F1 Score: ", f1_score(y_train_yes, y_train_pred))
+
+from sklearn.metrics import precision_recall_curve
+y_scores = cross_val_predict(sgd_clf, x_train, y_train_yes, cv=3, method="decision_function")
+
+precisions, recalls, thresholds = precision_recall_curve(y_train_yes, y_scores)
+
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+    plt.xlabel("Threshold")
+    plt.legend(loc="center left")
+    plt.ylim([0,1])
+
+# plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+
+def plot_precision_vs_recall(precisions, recalls):
+    plt.plot(recalls, precisions, "b-", linewidth=2)
+    plt.xlabel("Recall", fontsize=16)
+    plt.ylabel("Precision", fontsize=16)
+    plt.axis([0,1,0,1])
+
+
+plot_precision_vs_recall(precisions, recalls)
+plt.show()
